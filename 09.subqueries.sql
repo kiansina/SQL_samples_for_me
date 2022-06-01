e4683b053b8ef6b904b6092cbad2bbab66d1fcde
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-06-01 17:03:34 +0200

    Add files via upload

---
+++ 09.subqueries.sql
@@ -0,0 +1,63 @@
+create table racing (
+ make text,
+ model text,
+ year text,
+ price text
+);
+
+insert into racing (make, model, year, price) values ('Nissan', 'Stanza', 1990, 2000);
+insert into racing (make, model, year, price) values ('Dodge', 'Neon', 1995, 800);
+insert into racing (make, model, year, price) values ('Dodge', 'Neon', 1998, 2500);
+insert into racing (make, model, year, price) values ('Dodge', 'Neon', 1999, 3000);
+insert into racing (make, model, year, price) values ('Ford', 'Mustang', 2001, 1000);
+insert into racing (make, model, year, price) values ('Ford', 'Mustang', 2005, 2000);
+insert into racing (make, model, year, price) values ('Subaru', 'Impreza', 1997, 1000);
+insert into racing (make, model, year, price) values ('Mazda', 'Miata', 2001, 5000);
+insert into racing (make, model, year, price) values ('Mazda', 'Miata', 2001, 3000);
+insert into racing (make, model, year, price) values ('Mazda', 'Miata', 2001, 2500);
+insert into racing (make, model, year, price) values ('Mazda', 'Miata', 2002, 5500);
+insert into racing (make, model, year, price) values ('Opel', 'GT', 1972, 1500);
+insert into racing (make, model, year, price) values ('Opel', 'GT', 1969, 7500);
+insert into racing (make, model, year, price) values ('Opel', 'Cadet', 1973, 500);
+
+-- it can be done with more tables and IDs
+
+  select * from racing
+  where model ='Impreza';
+
+  select make from racing
+  where (year, price)=('1997','1000');
+
+  select make from racing
+  where (year) = (select year from racing where model='Impreza');
+
+
+
+
+  select year from racing
+  where model ='Impreza';
+
+  select make from racing
+  where year=('1997');
+
+  select make from racing
+  where (year) = (select year from racing where model='Impreza');
+
+
+
+
+
+-- do you remember below code? including 'having' ?
+-- Imagine there was not 'having' option, how could you query using subqueries?
+
+select count(model) as CntMdl, model from racing
+where year = '2001' group by model having count(model)>1;
+
+-- Using Subqueries:
+select CntMdl , model from
+(
+   select count(model) as CntMdl, model
+   from racing
+   where year='2001' group by model
+) as side_table
+where CntMdl>1;
